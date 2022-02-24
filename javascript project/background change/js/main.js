02729fea56590de03e9be94923f76bd9f42e3656
Aisha Mirza <84517249+ayshenm@users.noreply.github.com>
2022-02-24 15:56:53 +0300

    random and series background color change
    24.02.2022

--- javascript project/background change/js/main.js
+++ javascript project/background change/js/main.js
@@ -1,12 +1,24 @@
 const Body = document.querySelector("body");
 const Button = document.querySelector("button");
-const colors = ["yellow","green","blue","purple","black"];
+const colors = ["yellow","green","blue","purple","black","#85a832","#3c32a8","#3c32a8","#a8a032"];
 
 
 Button.addEventListener("click",changeBackground);
 
+ let series = 0;
+
 function changeBackground(){
-    console.log("tikadim");
-    const colorAdd = colors[3];
+    //random color
+    //const randomColors = Math.floor(Math.random() * colors.length);
+    //console.log(randomColors);
+    //const colorAdd = colors[randomColors];
+    //Body.style.backgroundColor = colorAdd;
+
+
+    //series color
+    
+    if(series == 8) series = 0
+    const colorAdd = colors[series];
+    series++;
     Body.style.backgroundColor = colorAdd;
 }
